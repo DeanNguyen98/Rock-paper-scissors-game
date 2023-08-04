1004c558abb475d589615e926e327ca55da67547
DeanNguyen98 <nctuan.hn@gmail.com>
2023-08-05 00:50:59 +1000

    Updated HTML file and Js file to interact with the UI

--- script2.js
+++ script2.js
@@ -1,35 +1,73 @@
-let myarray = ["rock", "paper", "scissors"];
-function getComputerchoice () {
-    return myarray[Math.floor(Math.random() * myarray.length)];
+let playerScore = 0;
+let computerScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+choices.forEach((choice) => {
+    choice.addEventListener("click" , () => {
+        const playerChoice = choice.getAttribute("data-choice");
+        matchChoices(playerChoice);
+
+    })
+
+})
+
+function matchChoices (playerChoice) {
+    const Array = ["rock" , "paper" , "scissors"];
+    const computerChoice = Array[Math.floor(Math.random() * Array.length)];
+    const displaymessage = document.getElementById("Display-msg")
+    console.log(`Player Choice : ${playerChoice}`);
+    console.log(`Computer Choice : ${computerChoice}`);
+    if (playerChoice == "rock" && computerChoice == "scissors" ||
+        playerChoice == "paper" && computerChoice == "rock" ||
+        playerChoice == "scissors" && computerChoice == "paper") {
+            displaymessage.textContent = "You win!Congrats";
+            playerScore ++;
+
+        } else if (playerChoice == computerChoice) {
+            displaymessage.textContent = "It's a draw!";
+        } else {
+            displaymessage.textContent = "You still got this! Try again!";
+            computerScore ++;
+        }
+        Score();
+
+}
+
+
+function Score () {
+    const playerScorepara = document.getElementById("player-score");
+    const computerScorepara = document.getElementById("computer-score"); 
+    let resultText;
+    playerScorepara.textContent = `${playerScore}`;
+    computerScorepara.textContent = `${computerScore}`;
+    if (playerScore >= 5) {
+        //create the modal with New Game button
+        const modal = document.getElementById('result-modal');
+        const resultTextContent = document.getElementById("result-text");
+        resultTextContent.textContent = "Congrats! You win the game!!";
+        const newGameButton = document.getElementById("new-game-button");
+        newGameButton.addEventListener("click", Gamereset);
+        modal.style.display = "flex";
+
+    } else if (computerScore >= 5) {
+        const modal = document.getElementById('result-modal');
+        const resultTextContent = document.getElementById("result-text");
+        resultTextContent.textContent = "Unlucky...Try again!";
+        const newGameButton = document.getElementById("new-game-button");
+        newGameButton.addEventListener("click", Gamereset);   
+        modal.style.display = "flex"; 
+    }
+}
+
+function Gamereset() {
+    playerScore = 0;
+    computerScore = 0;
+    document.getElementById("player-score").textContent = 0;
+    document.getElementById("computer-score").textContent = 0;
+    document.getElementById("result-modal").style.display ="none";
 }
 
 
 
-function playRound(playerSelection, computerSelection) {
 
-
-    console.log(`Player choice: ${playerSelection}`);
-console.log(`Computer Choice: ${computerSelection}`);
-    if (playerSelection === computerSelection) {
-        return string = "Draw";
-    } else if (playerSelection === "rock" && computerSelection === "paper" ||
-               playerSelection === "paper" && computerSelection === "rock" ||
-               playerSelection === "scissors" && computerSelection === "paper")
-               {
-                return string = " You Win! Congratulations!";
-               } else {
-
-                return string = "You lose";
-               }
-            
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("what is your choice(rock, paper, scissors?) :");
-    playRound(playerSelection, getComputerchoice());
-}
-console.log('Game over');
-}
-game();
 
